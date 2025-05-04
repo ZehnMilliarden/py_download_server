2824c2b66a09d9c1db0a883b84478d61110556e1
ZehnMilliarden <git@lxf.science>
2025-05-04 20:57:24 +0800

    [pds] 修复一些问题

--- pds.py
+++ pds.py
@@ -1,4 +1,4 @@
-from .src.server import Server
+from src.server import Server
 
 if __name__ == '__main__':
     server = Server()
--- src/server.py
+++ src/server.py
@@ -10,8 +10,8 @@ class RequestHandler(http.server.SimpleHTTPRequestHandler):
         else:
             http.server.SimpleHTTPRequestHandler.do_GET(self)
 
-    def list_files(self):
-        files = [f for f in os.listdir(self.server.server.working_dir) if os.path.isfile(os.path.join(self.server.server.working_dir, f))]
+    def list_files(self, folder = './'):
+        files = [f for f in os.listdir(folder) if os.path.isfile(os.path.join(folder, f))]
         files_html = '<html><body><h1>Available Files</h1><ul>' + ''.join([f'<li><a href="{f}">{f}</a></li>' for f in files]) + '</ul></body></html>'
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
@@ -19,10 +19,17 @@ class RequestHandler(http.server.SimpleHTTPRequestHandler):
         self.wfile.write(bytes(files_html, 'utf-8'))
 
 class Server(socketserver.TCPServer):
-    def __init__(self, host='localhost', port=8080, working_dir='.'):
+    def __init__(self, host='localhost', port=8080, working_dir='./'):
         self.working_dir = working_dir
-        super().__init__((host, port), RequestHandler)
-        self.RequestHandler.server = self
+        super().__init__((host, port), RequestHandler, bind_and_activate=False)
+        self.server_name = self.server_address[0]
+        self.allow_reuse_address = True
+        self.RequestHandlerClass.server_address = self.server_address
+        self.RequestHandlerClass.protocol_version = "HTTP/1.0"
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.server_bind()
+        self.server_activate()
 
     def upload(self, filename, target_dir):
         file_path = os.path.join(self.working_dir, target_dir, filename)
